ad4ce87d5f911bd188d8c119f5773c4832658fe8
Higgins, Joshua <Joshua.Higgins@sungard.com>
2015-03-10 15:31:39 -0400

    basic initial value test on PackeryController

--- test/unit/controllers/PackeryController.spec.js
+++ test/unit/controllers/PackeryController.spec.js
@@ -2,4 +2,21 @@ describe('Controllers: PackeryController', function () {
 
   beforeEach(module('angular-packery'));
 
+  var scope, vm;
+
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    vm = $controller('PackeryController', {
+      $scope: scope
+    });
+  }));
+
+  it('has correct initial values', function () {
+    expect(vm.packeryInstantiated).toBe(false);
+    expect(vm.packeryDraggable).toBe(false);
+    expect(vm.dragHandle).toBe(undefined);
+    expect(typeof vm.uniqueId).toBe('number');
+    expect(vm.packery).toEqual({});
+  });
+
 });
